70078202bdd6318dc5870e26366741c3442dc00a
Michel <michelaurier@outlook.com>
2024-01-29 14:22:38 +0100

    Update Setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,23 @@ Created on Mon Jan 29 13:57:06 2024
 
 @author: Michel
 """
-
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
+
 setup(
- name='Quiz-game-AURIER-CDOF4',
- version='1.0',
- author='aurier93',
- license='MIT',
- long_description=open('README.md').read(),
+    name='QuizGame',
+    version='0.1.0',
+    py_modules=['QuizGame'],
+    author='Michel',
+    author_email='your.email@example.com',
+    description='The player answers a number of questions to try and win the game.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/aurier93/Quiz-game-AURIER-CDOF4',
+    license='MIT',
+    python_requires='>=3.6',
+    classifiers=[
+       
+    ],
 )
+
