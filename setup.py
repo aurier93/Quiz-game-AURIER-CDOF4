9f82c5c6d05d695fd0fd0c40e652fd465d9c1378
Michel <michelaurier@outlook.com>
2024-01-29 14:01:29 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 13:57:06 2024
+
+@author: Michel
+"""
+
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='Quiz-game-AURIER-CDOF4',
+ version='1.0',
+ author='aurier93',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
